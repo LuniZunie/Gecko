c4b9e33cd318fdd66e8786510ec0972f815150ee
LuniZunie <cedric.hotopp@gmail.com>
2023-01-26 14:39:43 +0000

    .

--- host.js
+++ host.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const ejs = require('ejs');
+const express = require("express");
+const ejs = require("ejs");
 // Initialise Express
 const app = express();
 // Render static files
-app.use(express.static('views/partials'));
+app.use(express.static("views/partials"));
 
 // Set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // Port website will run on
-app.listen(3030) // This is actually the port it will listen on, default 8080 as of now.
-console.log("Listening on port 8080. Check code for more info.");
+app.listen(3030);
+console.log("Listening on port 3030. Check code for more info.");
 
 
 // *** GET Routes - display pages ***
 // Root Route
-app.get('/', function (req, res) {
-    res.redirect('/game');
+app.get("/", function (req, res) {
+    res.redirect("/game");
 });
-app.get('/game', function (req, res) {
-    res.render('pages/game/HTML/Index.ejs');
+app.get("/game", function (req, res) {
+    res.render("pages/game/HTML/Index.ejs");
 });
 app.use((req, res, next) => {
-    res.status(404).redirect('/');
+    res.status(404).redirect("/");
 });
